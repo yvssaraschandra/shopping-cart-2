f860be166494edb3116121201f03685fe7b94f92
Sarat Chandra <yvssaraschandra@gmail.com>
2020-05-27 16:37:38 +0530

    Updates

---
+++ precache-manifest.79754b233296fd95e8bbe27ab89b56bb.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a1dadbd3db4cd6a8fd867215493b234",
+    "url": "/shopping-cart-2/index.html"
+  },
+  {
+    "revision": "5d763be25c5025ede7ef",
+    "url": "/shopping-cart-2/static/css/main.dfe17f73.chunk.css"
+  },
+  {
+    "revision": "017a3c98cd4c51486556",
+    "url": "/shopping-cart-2/static/js/2.9393bbf4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/shopping-cart-2/static/js/2.9393bbf4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d763be25c5025ede7ef",
+    "url": "/shopping-cart-2/static/js/main.e34dcb55.chunk.js"
+  },
+  {
+    "revision": "30a3a3089492a0d2fc82",
+    "url": "/shopping-cart-2/static/js/runtime-main.7872fe00.js"
+  },
+  {
+    "revision": "0af514115218a7255c30e490daafcae5",
+    "url": "/shopping-cart-2/static/media/item2.0af51411.jpg"
+  },
+  {
+    "revision": "dcb0a969714216de716c5c8cba8e3a49",
+    "url": "/shopping-cart-2/static/media/item3.dcb0a969.jpg"
+  },
+  {
+    "revision": "2f823d4a2aa349c586610b8ec8b36abe",
+    "url": "/shopping-cart-2/static/media/item5.2f823d4a.jpg"
+  }
+]);
